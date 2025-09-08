818074b9edb0f5fd2d87a897549908a530ee1faa
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-08 22:27:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function firstChar(text) {
-	let arr = text.split("");
-	return arr[0];
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " ") {
+      return text[i];  
+    }
+  }
+  return ""; 
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
+// Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
 alert(firstChar(text));
